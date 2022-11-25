a1cda45cc1c824608855011b5d6da02d0414cbf4
constant-inos <kon.karageorgos.g@gmail.com>
2022-11-25 16:30:31 +0200

    aa

--- controllers/controller_pick_n_drop_3.py
+++ controllers/controller_pick_n_drop_3.py
@@ -1,5 +1,6 @@
 import rospy
-from sensor_msgs.msg import LaserScan, Range, Image
+from sensor_msgs.msg import LaserScan, Range, Image, JointState
+from control_msgs.msg import JointTrajectoryControllerState
 from std_msgs.msg import String
 from std_msgs.msg import Float64
 import socket, pickle
@@ -7,14 +8,12 @@ import numpy as np
 import time
 import math
 import cv2
+from cv_bridge import CvBridge
+import random
 
-pose_message=[]
-before_pose_message=[]
-lock=0
-interupted=False
-saved_state=[]
-BEFORE_READING=0
+bridge = CvBridge()
 sub=0
+sub1 = 0
 port = 9988
 
 def move_to_specified_pose(value):
@@ -41,12 +40,6 @@ def control_motor_angles(value):
 
 def move_gripper(value):
     global port
-    # gripper: left_finger, right_finger
-    #self.group2.set_joint_value_target([0.02800624132752438, -0.02622681833090827])
-    #self.group2.set_joint_value_target([0.028, -0.026]) #very open
-    #self.group2.set_joint_value_target([0.018, -0.016]) #almost closed
-    #self.group2.set_joint_value_target([0.034, -0.034]) #almost totally open
-    #self.group2.set_joint_value_target([0.016, -0.016]) #good for cube
     arr=[]
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.connect(("localhost", 9988))
@@ -56,7 +49,10 @@ def move_gripper(value):
     s.send(data_string)
     s.close()
 
-move_gripper([0.033, -0.033])
+def open_gripper():
+    move_gripper([0.033, -0.033])
+def close_gripper():
+    move_gripper([0.016, -0.016])
 
 
 def ranges_to_polar(ranges,object_r=0.025,angle_inc=0.031733):
@@ -73,89 +69,6 @@ def ranges_to_polar(ranges,object_r=0.025,angle_inc=0.031733):
     d = np.mean(cut_ranges[:3]) + object_r
     theta = np.mean(index_cut_ranges)*angle_inc
     return (d,theta)
-
-
-def callback(msg):
-    start_index=0
-    end_index=0
-    global before_pose_message
-    global lock
-    global interupted
-    global saved_state
-    global BEFORE_READING
-    global pose_message
-    flag=0
-
-
-
-
-    if d<2.0 and round(d,1)!=round(BEFORE_READING,1):
-       #print("aaa")
-       BEFORE_READING=d
-       amplitude= 1.0*d
-       y=round(amplitude*math.sin(theta),5)
-       x=round(amplitude*math.cos(theta),5)
-       if theta <= np.pi/2:
-          offset=0.1
-          phi=np.arcsin((y+offset)/(np.sqrt(((y+offset)**2)+((x)**2))))
-       if theta > np.pi/2:
-          offset=0.1
-          pi_minus_theta=np.pi-theta
-          pi_minus_phi=np.arcsin((y+offset)/(np.sqrt(((y+offset)**2)+((-x)**2))))
-          phi=np.pi-pi_minus_phi
-       #print("theta:",theta,"phi:",phi)
-       pose_message=[x+0.0+0.0000001,y+offset+0.0000001,0.2,0.0,0.0,phi]
-    if d>=2.0 and round(d,1)!=round(BEFORE_READING,1):
-       #print("bbb")
-       BEFORE_READING=d
-       print(d)
-       pose_message=[0.1,0.0,0.25,0.0,0.0,0.0]
-    if pose_message!=before_pose_message:
-       #print("ccc")
-       print(d)
-       before_pose_message=pose_message[:]
-       if lock==0:
-         move_to_specified_pose(pose_message)
-         lower=pose_message[:]
-         lower[2]=0.14
-         move_to_specified_pose(lower)
-         move_gripper([0.016, -0.016])
-         move_to_specified_pose(pose_message)
-         move_to_specified_pose([0.1,0.0,0.25,0.0,0.0,0.0])
-         before_pose_message=[0.1,0.0,0.25,0.0,0.0,0.0]
-         move_gripper([0.033, -0.033])
-         #rospy.signal_shutdown("whatever")
-         #main()
-         sub.unregister()
-         main()
-         #sub=rospy.Subscriber("/wx200/sensor/sonar_front4", LaserScan, callback) # cube big range sensor
-       if lock==1:
-         interupted=True
-         saved_state=pose_message
-    if pose_message==before_pose_message and interupted==False:
-       pass
-    if pose_message==before_pose_message and interupted==True and lock==0:
-       move_to_specified_pose(saved_state)
-       #print("BBBBBBBBBBBBBBBBBBBBBBBBBBBBB")
-       lower=saved_state[:]
-       lower[2]=0.14
-       move_to_specified_pose(lower)
-       move_gripper([0.016, -0.016])
-       move_to_specified_pose(pose_message)
-       move_to_specified_pose([0.1,0.0,0.25,0.0,0.0,0.0])
-       before_pose_message=[0.1,0.0,0.25,0.0,0.0,0.0]
-       move_gripper([0.033, -0.033])
-       #rospy.signal_shutdown("whatever")
-       #main()
-       sub.unregister()
-       main()
-       #sub=rospy.Subscriber("/wx200/sensor/sonar_front4", LaserScan, callback) # cube big range sensor
-       saved_state=[]
-       interupted=False
-
-
-from cv_bridge import CvBridge
-bridge = CvBridge()
 
 def get_red_pixels(cv_image):
     img_hsv=cv2.cvtColor(cv_image, cv2.COLOR_BGR2HSV)
@@ -189,8 +102,6 @@ def get_target(red_pixels):
 
     x,y = pixel2coords(px,py)
 
-    print(px,py)
-    print(x,y)
     return x,y
 
 def pixel2coords(px,py,H=480,W=640):
@@ -201,6 +112,11 @@ def pixel2coords(px,py,H=480,W=640):
 
     x = -Xcv + C*H/2
     y = -Ycv + (0.54 + C*W/2.0)
+
+    # small perception corrections
+    x = x + 0.025*x/0.4
+    y = y - 0.025*(0.54-y)/0.25
+
     return x,y
 
 q = []
@@ -208,44 +124,63 @@ busy = False
 standby_pose = [0.1,0.0,0.25,0.0,0.0,0.0]
 
 def read_camera(msg):
+    global standby_pose
     global busy
-    print(busy)
     if busy: return
+
+    move_to_specified_pose(standby_pose)
 
     cv_image = bridge.imgmsg_to_cv2(msg, desired_encoding='bgr8')
     red_pixels = get_red_pixels(cv_image)
     x,y = get_target(red_pixels)
-    print(x,y)
 
-    if x<0:
-        phi = np.pi - np.arctan(-y/x)
-    else:
-        phi = np.arctan(y/x)
+    if x<0: phi = np.pi - np.arctan(-y/x)
+    else: phi = np.arctan(y/x)
 
     pose_message=[x+0.0+0.0000001,y+0.0000001,0.2,0.0,0.0,phi]
 
+    xr = random.random()-0.5
+    yr = random.random()*0.35+0.2
+    if x<0: phir = np.pi - np.arctan(-yr/xr)
+    else: phir = np.arctan(yr/xr)
+    random_pose=[xr+0.0+0.0000001,yr+0.0000001,0.2,0.0,0.0,phir]
+
     busy = True
     move_to_specified_pose(pose_message)
-    # lower=pose_message[:]
-    # lower[2]=0.14
-    # move_to_specified_pose(lower)
-    # move_gripper([0.016, -0.016])
-    # move_to_specified_pose(pose_message)
-    # move_to_specified_pose(standby_pose)
-    # busy = False
-    # move_gripper([0.033, -0.033])
-    # print('end',busy)
-
+    lower=pose_message[:]
+    lower[2]=0.14
+    move_to_specified_pose(lower)
+    close_gripper()
+    move_to_specified_pose(pose_message)
+    print(xr,yr)
+    # move_to_specified_pose(random_pose)
+    open_gripper()
+    move_to_specified_pose(standby_pose)
+    busy = False
     sub.unregister()
     main()
 
+def is_busy(msg):
+    global sub1
 
+    d = math.dist(msg.actual.positions,msg.desired.positions)
+    # print(d)
+    # print(msg.desired.positions)
+    # print(msg.actual.positions)
 
+def execution_status(msg):
+    global sub2
+    print(msg)
+
+from moveit_msgs.msg import MoveGroupResult
+from actionlib_msgs.msg import GoalStatus
 def main():
-    global sub
+    global sub,sub1,sub2
     rospy.init_node('robot_state_publisher')#this is an existing topic
     #sub=rospy.Subscriber("/wx200/sensor/sonar_front4", LaserScan, callback) # cube big range sensor
     sub=rospy.Subscriber("/wx200/camera_link_optical/image_raw", Image, read_camera) # Camera sensor, Image is the data type sensor_msgs/Image
+    # sub1 = rospy.Subscriber("/wx200/arm_controller/state", JointTrajectoryControllerState, is_busy)
+    sub2 = rospy.Subscriber("/wx200/move_group/result", MoveGroupResult, execution_status)
     rospy.spin()
 
 if __name__ == "__main__":
