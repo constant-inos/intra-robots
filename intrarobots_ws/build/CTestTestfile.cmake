8423840c0ed95992906876970f05b259f7a2727c
constant-inos <kon.karageorgos.g@gmail.com>
2022-12-23 16:19:43 +0200

    MerryChristmas

---
+++ intrarobots_ws/build/CTestTestfile.cmake
@@ -0,0 +1,12 @@
+# CMake generated Testfile for 
+# Source directory: /home/karagk/Workspaces/intrarobots_ws/src
+# Build directory: /home/karagk/Workspaces/intrarobots_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("robotic_arm_moveit_interface")
+subdirs("robotic_arm_descriptions")
+subdirs("robotic_arm_gazebo")
+subdirs("robotic_arm_moveit")
+subdirs("master")
---
+++ intrarobots_ws/build/master/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/karagk/Workspaces/intrarobots_ws/src/master
+# Build directory: /home/karagk/Workspaces/intrarobots_ws/build/master
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
